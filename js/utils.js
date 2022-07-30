1c5ec9f6b4ffc695728e0fad42e0dce8bac42f33
DanLowo <danlowo@gmail.com>
2022-07-30 14:23:27 +0100

    add COMPONENT_URL const

--- js/utils.js
+++ js/utils.js
@@ -2,6 +2,7 @@
 const HOST_URL = "http://127.0.0.1:5500"
 const DESKTOP_TEMPLATE_URL = HOST_URL.concat("/html/template/desktop-template.html")
 const MOBILE_TEMPLATE_URL = HOST_URL.concat("/html/template/mobile-template.html")
+const COMPONENTS_URL = HOST_URL.concat("/html/components.html")
 
 // Send a get request to an html file, then parse the contents to become an HTML DOM Element / Node
 const getHTMLFromURL = async (url) => {
