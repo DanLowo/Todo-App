b0288d4b486e06c9027d9666a6e16926f4c4f229
DanLowo <danlowo@gmail.com>
2022-07-30 11:42:00 +0100

    script to load site template based on device type

---
+++ js/loadSiteTemplate.js
@@ -0,0 +1,23 @@
+const loadSiteByDeviceType = async () => {
+  const isDeviceMobile = isDeviceTypeMobile();
+  
+  try {
+    const desktopTemplate = await getHTMLFromURL(DESKTOP_TEMPLATE_URL);
+    const mobileTemplate = await getHTMLFromURL(MOBILE_TEMPLATE_URL);
+
+    // Add template to DOM based on device type
+    if (isDeviceMobile) {
+      const mobileTemplateSection = mobileTemplate.body.firstElementChild;
+      document.body.insertAdjacentElement("afterbegin", mobileTemplateSection);
+    } else {
+      const desktopTemplateSection = desktopTemplate.body.firstElementChild;
+      document.body.insertAdjacentElement("afterbegin", desktopTemplateSection);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+loadSiteByDeviceType()
+  .then()
+  .catch((err) => console.log(err));
