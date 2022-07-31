878d7ac9ff2d4038b0e1e2868fb5cd29755032dd
DanLowo <danlowo@gmail.com>
2022-07-31 08:51:32 +0100

    filter tasks funtions, generate random colors..

--- js/utils.js
+++ js/utils.js
@@ -1,14 +1,25 @@
 // Default Values
-const HOST_URL = "http://127.0.0.1:5500"
-const DESKTOP_TEMPLATE_URL = HOST_URL.concat("/html/template/desktop-template.html")
-const MOBILE_TEMPLATE_URL = HOST_URL.concat("/html/template/mobile-template.html")
-const COMPONENTS_URL = HOST_URL.concat("/html/components.html")
+const tasksStatus = {
+  all: "ALL",
+  pending: "PENDING",
+  deleted: "DELETED",
+  completed: "COMPLETED",
+};
+
+const HOST_URL = "http://127.0.0.1:5500";
+const DESKTOP_TEMPLATE_URL = HOST_URL.concat(
+  "/html/template/desktop-template.html"
+);
+const MOBILE_TEMPLATE_URL = HOST_URL.concat(
+  "/html/template/mobile-template.html"
+);
+const COMPONENTS_URL = HOST_URL.concat("/html/components.html");
 
 // Send a get request to an html file, then parse the contents to become an HTML DOM Element / Node
 const getHTMLFromURL = async (url) => {
   try {
     const data = await fetch(url);
-    const parser = new DOMParser()
+    const parser = new DOMParser();
     const parsedHTML = parser.parseFromString(await data.text(), "text/html");
 
     return parsedHTML;
@@ -18,9 +29,47 @@ const getHTMLFromURL = async (url) => {
 };
 
 const isDeviceTypeMobile = () => {
-  const size = window.innerWidth
-  if(size <= 600) {
+  const size = window.innerWidth;
+  if (size <= 600) {
     return true;
   }
   return false;
-}
+};
+
+const generateRandomColor = () => {
+  const COLORS = [
+    "rgba(203,210,239,255)",
+    "rgba(207,224,208,255)",
+    "rgba(237,206,204,255)",
+    "rgb(143, 37, 12, 0.514)",
+    "rgb(16, 0, 7, 0.514)",
+    "rgb(231, 226, 71, 0.514)",
+    "rgb(204, 51, 99, 0.514)",
+    "rgb(33, 104, 105, 0.514)",
+    "rgb(141, 106, 159, 0.514)",
+    "rgba(28, 49, 68, 0.466)",
+  ];
+  const random = Math.round(Math.random() * 10);
+
+  return COLORS[random];
+};
+
+const filterTaskByStatus = (allTasks, type) => {
+  if (type === tasksStatus.all) {
+    return allTasks;
+  }
+
+  if(type === tasksStatus.deleted) {
+    return []
+  }
+
+  return (filterdTasks = allTasks.filter((task) => task?.status === type));
+};
+
+const filterStaredTasks = (allTasks) => {
+  return (staredItems = allTasks.filter((task) => task?.stared));
+};
+
+const filterNonStaredTasks = (allTasks) => {
+  return (staredItems = allTasks.filter((task) => !task?.stared));
+};
