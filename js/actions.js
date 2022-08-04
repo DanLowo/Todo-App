b845cae89b68e447267799851ddadc99417bef72
DanLowo <danlowo@gmail.com>
2022-08-04 07:18:21 +0100

    restructure fab actions & form

--- js/actions.js
+++ js/actions.js
@@ -1,4 +1,4 @@
-const changeFabButtonIcon = (isDialogOpen) => {
+const isShowFabButtonIcon = (isDialogOpen) => {
   const fabButtonIcon = document.querySelector("#fab i");
 
   if (isDialogOpen) {
@@ -8,13 +8,31 @@ const changeFabButtonIcon = (isDialogOpen) => {
   }
 };
 
-const openCreateListDialog = () => {
-  const dialogBox = document.querySelector("#dialog-box");
-  dialogBox.classList.toggle("closed");
+const openCreateListDialog = (dialogBox) => {
+  dialogBox.classList.remove("closed");
 
   const darkendBroundground = document.querySelector("#darken-background");
-  darkendBroundground.classList.toggle("closed");
+  darkendBroundground.classList.remove("closed");
 
-  // use nagation because if it doesn't contain closed then it is opened 
-  changeFabButtonIcon(!dialogBox.classList.contains("closed"));
+  isShowFabButtonIcon(true);
 };
+
+const closeCreateListDialog = (dialogBox) => {
+  dialogBox.classList.add("closed");
+
+  const darkendBroundground = document.querySelector("#darken-background");
+  darkendBroundground.classList.add("closed");
+
+  isShowFabButtonIcon(false);
+}
+
+const setCreateListDialog = () => {
+  const dialogBox = document.querySelector("#dialog-box");
+
+  if(dialogBox.classList.contains("closed")) {
+    openCreateListDialog(dialogBox)
+    return;
+  }
+
+  closeCreateListDialog(dialogBox)
+}
