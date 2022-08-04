cbc10e4c25db5d51b5f8323fe14548f5a324f4e5
DanLowo <danlowo@gmail.com>
2022-08-04 05:32:06 +0100

    fix fab open and close logic

--- js/actions.js
+++ js/actions.js
@@ -2,9 +2,9 @@ const changeFabButtonIcon = (isDialogOpen) => {
   const fabButtonIcon = document.querySelector("#fab i");
 
   if (isDialogOpen) {
-    fabButtonIcon.className = "fal fa-plus";
-  } else {
     fabButtonIcon.className = "fal fa-times opened";
+  } else {
+    fabButtonIcon.className = "fal fa-plus";
   }
 };
 
@@ -15,5 +15,6 @@ const openCreateListDialog = () => {
   const darkendBroundground = document.querySelector("#darken-background");
   darkendBroundground.classList.toggle("closed");
 
+  // use nagation because if it doesn't contain closed then it is opened 
   changeFabButtonIcon(!dialogBox.classList.contains("closed"));
 };
